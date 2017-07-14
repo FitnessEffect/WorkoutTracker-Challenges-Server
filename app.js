eed372b81a9427bab2b4e627cadf97611884d2b4
Stefan Auvergne <stefan.auvergne@gmail.com>
2017-07-14 13:33:51 -0400

    notification from client exercise issue fix

--- app.js
+++ app.js
@@ -40,10 +40,16 @@ application.post('/challenges', function(req, res, next) {
     var userID = req.body.userID || '';
     var opponentEmail = req.body.opponentEmail || '';
     var challengerEmail = req.body.userEmail || '';
+    var clientKey = req.body.clientKey || '';
+
+    console.log("clientKey");
+    console.log(clientKey);
 
 
     var formattedEmail = formateEmail(challengerEmail);
     var formattedOpponentEmail = formateEmail(opponentEmail);
+    console.log("FORMATTED OPPONENT EMAIL");
+    console.log(formattedOpponentEmail);
     var exName = "";
     var exDesc = "";
 
@@ -56,86 +62,169 @@ application.post('/challenges', function(req, res, next) {
        return tempEmail
    }
 
-    db.ref().child("users").child(userID).child("Exercises").child(exerciseKey).once("value", function(snapshot) {
-        var e = snapshot.val();
-        exerciseRetrieved = e;
-        //var keys = Object.keys(e);
+   //CHECK IF EXERCISE IS RETRIEVED FROM USER EXERCISES OR CLIENT EXERCISES
+    if (clientKey == " "){
+      db.ref().child("users").child(userID).child("Exercises").child(exerciseKey).once("value", function(snapshot) {
+          var e = snapshot.val();
+          exerciseRetrieved = e;
+          //var keys = Object.keys(e);
+          console.log("exercise from user");
+          console.log(exerciseRetrieved);
+      db.ref().child("emails").child(formattedOpponentEmail).once("value", function(snapshot){
+        var userKey = snapshot.val()
+        console.log(userKey);
 
-        console.log(formattedOpponentEmail);
-        console.log(exerciseRetrieved);
-    db.ref().child("emails").child(formattedOpponentEmail).once("value", function(snapshot){
-      var userKey = snapshot.val()
-      console.log(userKey);
-
-      db.ref().child("users").child(userKey).child("Challenges").child(exerciseKey).update(exerciseRetrieved);
-    });
+        db.ref().child("users").child(userKey).child("Challenges").child(exerciseKey).update(exerciseRetrieved);
+      });
 
 
-    db.ref().child("notification").child(formattedOpponentEmail).once("value", function(snapshot){
-      var num = snapshot.val();
-      intNum = parseInt(num, 10);
+      db.ref().child("notification").child(formattedOpponentEmail).once("value", function(snapshot){
+        var num = snapshot.val();
+        intNum = parseInt(num, 10);
 
-      //if (isNaN(parsedNum)) return;
+        //if (isNaN(parsedNum)) return;
 
-      intNum = intNum + 1;
-      db.ref().child("notification").update({[formattedOpponentEmail]:intNum});
-    });
+        intNum = intNum + 1;
+        db.ref().child("notification").update({[formattedOpponentEmail]:intNum});
+      });
 
-        //Set up apn with the APNs Auth Key
-        var apnProvider = new apn.Provider({
-            token: {
-                key: 'apns.p8', // Path to the key p8 file
-                keyId: '35WXBSSCNU', // The Key ID of the p8 file (available at https://developer.apple.com/account/ios/certificate/key)
-                teamId: 'JF325HQHWJ', // The Team ID of your Apple Developer Account (available at https://developer.apple.com/account/#/membership/)
-            },
-            production: false // Set to true if sending a notification to a production iOS app
-        });
+          //Set up apn with the APNs Auth Key
+          var apnProvider = new apn.Provider({
+              token: {
+                  key: 'apns.p8', // Path to the key p8 file
+                  keyId: '35WXBSSCNU', // The Key ID of the p8 file (available at https://developer.apple.com/account/ios/certificate/key)
+                  teamId: 'JF325HQHWJ', // The Team ID of your Apple Developer Account (available at https://developer.apple.com/account/#/membership/)
+              },
+              production: false // Set to true if sending a notification to a production iOS app
+          });
 
-        //perform look up using email to find key
-        db.ref().child("token").once("value", function(snapshot) {
-            var emails = snapshot.val();
-            console.log("DICTIONARY");
-            console.log(emails);
+          //perform look up using email to find key
+          db.ref().child("token").once("value", function(snapshot) {
+              var emails = snapshot.val();
+              console.log("DICTIONARY");
+              console.log(emails);
 
-            let deviceToken = emails[formattedOpponentEmail];
+              let deviceToken = emails[formattedOpponentEmail];
 
-            console.log(deviceToken);
-            // Prepare a new notification
-            var notification = new apn.Notification();
+              console.log(deviceToken);
+              // Prepare a new notification
+              var notification = new apn.Notification();
 
-            // Specify your iOS app's Bundle ID (accessible within the project editor)
-            notification.topic = 'com.stefan.auvergne.WorkoutTracker';
+              // Specify your iOS app's Bundle ID (accessible within the project editor)
+              notification.topic = 'com.stefan.auvergne.WorkoutTracker';
 
-            // Set expiration to 1 hour from now (in case device is offline)
-            notification.expiry = Math.floor(Date.now() / 1000) + 3600;
+              // Set expiration to 1 hour from now (in case device is offline)
+              notification.expiry = Math.floor(Date.now() / 1000) + 3600;
 
-            // Set app badge indicator
-            notification.badge = intNum;
+              // Set app badge indicator
+              notification.badge = intNum;
 
-            // Play ping.aiff sound when the notification is received
-            notification.sound = 'ping.aiff';
+              // Play ping.aiff sound when the notification is received
+              notification.sound = 'ping.aiff';
 
-            // Display the following message (the actual notification text, supports emoji)
+              // Display the following message (the actual notification text, supports emoji)
 
-            //pull email out and send
-            notification.alert = challengerEmail + " sent you a challenge!";
+              //pull email out and send
+              notification.alert = challengerEmail + " sent you a challenge!";
 
-            // Send any extra payload data with the notification which will be accessible to your app in didReceiveRemoteNotification
-            notification.payload = {
-                exercise: exerciseRetrieved
-            };
+              // Send any extra payload data with the notification which will be accessible to your app in didReceiveRemoteNotification
+              notification.payload = {
+                  exercise: exerciseRetrieved
+              };
 
-            // Actually send the notification
-            apnProvider.send(notification, deviceToken).then(function(result) {
-                // Check the result for any failed devices
-                console.log(JSON.stringify(result));
-                console.log(result);
-            });
+              // Actually send the notification
+              apnProvider.send(notification, deviceToken).then(function(result) {
+                  // Check the result for any failed devices
+                  console.log(JSON.stringify(result));
+                  console.log(result);
+              });
 
-            //server response
-            res.status(200).send();
-            });
-        });
+              //server response
+              res.status(200).send();
+              });
+          });
+    }else{
+      db.ref().child("users").child(userID).child("Clients").child(clientKey).child("Exercises").child(exerciseKey).once("value", function(snapshot) {
+          var e = snapshot.val();
+          exerciseRetrieved = e;
+          //var keys = Object.keys(e);
+            console.log("exercise from client");
+          console.log(exerciseRetrieved);
+      db.ref().child("emails").child(formattedOpponentEmail).once("value", function(snapshot){
+        var userKey = snapshot.val()
+        console.log(userKey);
+
+        db.ref().child("users").child(userKey).child("Challenges").child(exerciseKey).update(exerciseRetrieved);
+      });
+
+
+      db.ref().child("notification").child(formattedOpponentEmail).once("value", function(snapshot){
+        var num = snapshot.val();
+        intNum = parseInt(num, 10);
+
+        //if (isNaN(parsedNum)) return;
+
+        intNum = intNum + 1;
+        db.ref().child("notification").update({[formattedOpponentEmail]:intNum});
+      });
+
+          //Set up apn with the APNs Auth Key
+          var apnProvider = new apn.Provider({
+              token: {
+                  key: 'apns.p8', // Path to the key p8 file
+                  keyId: '35WXBSSCNU', // The Key ID of the p8 file (available at https://developer.apple.com/account/ios/certificate/key)
+                  teamId: 'JF325HQHWJ', // The Team ID of your Apple Developer Account (available at https://developer.apple.com/account/#/membership/)
+              },
+              production: false // Set to true if sending a notification to a production iOS app
+          });
+
+          //perform look up using email to find key
+          db.ref().child("token").once("value", function(snapshot) {
+              var emails = snapshot.val();
+              console.log("DICTIONARY");
+              console.log(emails);
+
+              let deviceToken = emails[formattedOpponentEmail];
+
+              console.log(deviceToken);
+              // Prepare a new notification
+              var notification = new apn.Notification();
+
+              // Specify your iOS app's Bundle ID (accessible within the project editor)
+              notification.topic = 'com.stefan.auvergne.WorkoutTracker';
+
+              // Set expiration to 1 hour from now (in case device is offline)
+              notification.expiry = Math.floor(Date.now() / 1000) + 3600;
+
+              // Set app badge indicator
+              notification.badge = intNum;
+
+              // Play ping.aiff sound when the notification is received
+              notification.sound = 'ping.aiff';
+
+              // Display the following message (the actual notification text, supports emoji)
+
+              //pull email out and send
+              notification.alert = challengerEmail + " sent you a challenge!";
+
+              // Send any extra payload data with the notification which will be accessible to your app in didReceiveRemoteNotification
+              notification.payload = {
+                  exercise: exerciseRetrieved
+              };
+
+              // Actually send the notification
+              apnProvider.send(notification, deviceToken).then(function(result) {
+                  // Check the result for any failed devices
+                  console.log(JSON.stringify(result));
+                  console.log(result);
+              });
+
+              //server response
+              res.status(200).send();
+              });
+          });
+    }
+
 });
 
 application.post("/ping", function(req, res, next) {
